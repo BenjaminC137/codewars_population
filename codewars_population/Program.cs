33682c25fda690d33ccad979e34cf5024493aaa3
benjaminc137 <benjamin.salasek@icloud.com>
2018-07-06 12:32:35 -0500

    initial

---
+++ codewars_population/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace codewars_population
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
