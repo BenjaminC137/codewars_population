6767d84f4535bd8d2c87065dc0a4fdd508964cb6
benjaminc137 <benjamin.salasek@icloud.com>
2018-07-06 14:18:16 -0500

    final success

---
+++ codewars_population/Arge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace codewars_population
+{
+    class Arge
+    {
+        public static int NbYear(int p0, double percent, int aug, int p)
+        {
+            //decimal newPercent = (Convert.ToDecimal(percent));
+            //newPercent *= 0.01m;
+            percent *= 0.01;
+            int years = 0;
+            do
+            {
+                //p0 += (int)(p0 * newPercent) + aug;
+                p0 += (int)(p0 * percent) + aug;
+                years++;
+            } while (p0 < p);
+            return years;
+        }
+    }
+}
+
+
